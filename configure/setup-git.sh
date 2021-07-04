263751161ffc15a91675087c9509d5296083729d
Joshua Hegedus <josh.hegedus@outlook.com>
2021-07-04 15:35:29 +0200

    Git user error fixed

--- configure/setup-git.sh
+++ configure/setup-git.sh
@@ -3,10 +3,14 @@
 read -p Email: GIT_EMAIL
 
 ssh-keygen -t ed25519 -C $GIT_EMAIL
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_ed25519
-xclip -selection clipboard < ~/.ssh/id_ed25519.pub
-echo "===> DONE."
-read -n 1 -s -r -p "Press any key to continue"
+sudo -u joshika45 -i eval $(ssh-agent -s)
+
+ssh-add /home/$USER//.ssh/id_ed25519
+xclip -selection clipboard < /home/$USER//.ssh/id_ed25519.pub
+
+echo "  --> SSH Public key copied."
+read -n 1 -s -r -p "Press any key, when pasting into github is done..."
 echo
-ssh -T git@github.com
+
+#ssh -T git@github.com
+echo "===> Done!"
