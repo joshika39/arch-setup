26b49dd6f51d9d591a48e3f1dc4f9d66eebeab63
Joshua Hegedus <josh.hegedus@outlook.com>
2021-07-04 15:41:08 +0200

    made a few fixes

--- packages/install-official-apps.sh
+++ packages/install-official-apps.sh
@@ -2,4 +2,26 @@
 
 echo "====> Downloading official apps"
 echo
-pacman -Sy --needed $(<pkg/official-apps.txt) 
+
+PKG_DOWNLOAD=""
+count=0
+VER=${1}
+# echo $VER
+# echo
+
+while read PKG
+do
+	if [[ ${PKG::1} != "#" && ${PKG::1} != "" ]]; then 
+	# echo "This is a package!" 
+	# echo ${count}
+		if [[ ${PKG:0:4} == "-lts" ]]; then
+			PKG_DOWNLOAD+="${PKG#*-lts}$VER"	
+		else	
+			PKG_DOWNLOAD+=" $PKG"
+		fi
+		((count+=1)) 
+	fi
+done < pkg/official-apps.txt
+
+# echo ${PKG_DOWNLOAD}
+pacman -Sy --needed ${PKG_DOWNLOAD}
